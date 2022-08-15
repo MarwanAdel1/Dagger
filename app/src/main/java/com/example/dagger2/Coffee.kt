8ab9241d7a5932957b2cbdae57acac7fbb7d2aff
Marwan Adel <marwan10025@gmail.com>
2022-08-15 09:05:05 +0200

    Method injection

--- app/src/main/java/com/example/dagger2/Coffee.kt
+++ app/src/main/java/com/example/dagger2/Coffee.kt
@@ -1,9 +1,14 @@
 package com.example.dagger2
 
+import android.util.Log
 import javax.inject.Inject
 
 class Coffee @Inject constructor(val farm: Farm, val river: River) {
     private val TAG by lazy { "Coffee" }
+    
+    init {
+        Log.i(TAG, "Coffee constructor is called")
+    }
 
     @Inject
     lateinit var myCustomFarm: Farm
@@ -11,9 +16,12 @@ class Coffee @Inject constructor(val farm: Farm, val river: River) {
     @Inject
     lateinit var myCustomRiver: River
 
+    @Inject
+    fun getCoffeeOrder() {
+        Log.i(TAG, "getCoffeeOrder")
+    }
 
     fun getCustomCoffee(): String {
         return myCustomFarm.getCustomFarm() + "+" + myCustomRiver.getCustomRiver()
     }
-
 }
